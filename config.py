bf65b1d75fd706d188834461d2c90f59fdb26c18
Atharv Patole <atharv@fultonring.com>
2025-11-18 17:21:21 -0500

    resolved token issue

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ INDEX_DIR = "index_store"
 EMBED_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 
 # Groq LLM model name (hosted; no GPU needed on your laptop)
-GROQ_MODEL_NAME = "llama3-8b-8192"
+GROQ_MODEL_NAME = "allam-2-7b"
 
 # how many chunks to retrieve per query
 TOP_K = 10
